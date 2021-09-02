3e4f9087e9fc8aba193de048633965ce544b09ef
Aleexmarcelo <83593099+Aleexmarcelo@users.noreply.github.com>
2021-09-01 21:18:39 -0300

    Add files via upload

---
+++ Pessoas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TarefaPessoas
+{
+    class Pessoas
+    {
+        //Instaciar a variavel
+        public string name;
+        public int idade;
+    }
+   
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace TarefaPessoas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Instaciar a variavel
+            Pessoas x;
+            Pessoas y;
+
+            //Instanciar a Variável
+            x = new Pessoas();
+            y = new Pessoas();
+
+            // Informação para o usuário digitar o nome e idade
+            Console.WriteLine("Diga seu nome");
+            x.name = (Console.ReadLine());
+            Console.WriteLine("Ok, seu nome é:", x);
+
+            Console.WriteLine("Agora me diga sua idade");
+            x.idade = int.Parse(Console.ReadLine());
+
+            // Informação para o usuário digitar o nome e idade
+            Console.WriteLine("Diga seu nome");
+            y.name = Console.ReadLine();
+            Console.WriteLine("Ok, seu nome é:", y);
+            Console.WriteLine("Agora me diga sua idade");
+            y.idade = int.Parse(Console.ReadLine());
+
+            //Comparação para saber quem é o mais velho
+            if
+                (x.idade > y.idade)
+
+                Console.WriteLine("O mais velho é Mario");
+
+            else
+                Console.WriteLine("O mais velho é Luigi");
+
+        }
+    }
+}
